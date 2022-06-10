ad738b407d6467671448771a3f2a6da405e2e953
Misha Hrysiuk <0973853103h@gmail.com>
2022-06-11 01:41:07 +0300

    update AboutUsPage

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -16,7 +16,7 @@ const AppHeader = () => {
                         <Link to="/" className="header__logo">SUBLIhome</Link>
                         <nav>
                             <Stack direction="horizontal" gap={5}>
-                                <NavLink end className = {({isActive}) => doActive(isActive)} to="/about_us">Про нас</NavLink>
+                                <NavLink end className = {({isActive}) => doActive(isActive)} to="/about_us">Про проект</NavLink>
                                 <NavLink end className = {({isActive}) => doActive(isActive)} to="/products">Наші товари</NavLink>
                                 <NavLink end className = {({isActive}) => doActive(isActive)} to="/cart">Корзина</NavLink>
                                 <NavLink end className = {({isActive}) => doActive(isActive)} to="/profile">Профіль</NavLink>
--- src/components/pages/AboutUsPage.js
+++ src/components/pages/AboutUsPage.js
@@ -1,11 +1,20 @@
+import { Card } from "react-bootstrap"
 import { authenticationService } from "../../services/auth-service"
 
 const AboutUsPage = () => {
     return (
         <div className="block">
             <div className="container">
-                <div className="block__title title">Про нас</div>
-                    <div className="block__row">
+                <div className="block__title title">Про проект</div>
+                <div className="block__row">
+                    <Card>
+                        <Card.Body style= {{textAlign: 'center'}}>
+                            <Card.Title>Про проект</Card.Title>
+                            <Card.Text>Цей проект на дипломну бакалаврську роботу підготував <br/>
+                                студент КПІ, ФІОТ, групи ІВ-83 <br/>
+                                Грисюк Михайло</Card.Text>
+                        </Card.Body>
+                    </Card>
                     </div>
             </div>
         </div>
